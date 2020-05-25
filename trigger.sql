ee4c02783177be09c75e6e8b9f8131f0773ccce1
OlegKass <44788664+OlegKass@users.noreply.github.com>
2020-05-25 12:36:38 +0300

    Delete trigger.sql

--- trigger.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE TRIGGER check_age
-BEFORE INSERT
-    ON laureat
-    FOR EACH ROW
-BEGIN
-    IF (:new.reward_year - extract(year from  :new.birth_date) < 35 ) THEN
-        RAISE_APPLICATION_ERROR(-20002, 'Too young');
-    END IF;
-END;
